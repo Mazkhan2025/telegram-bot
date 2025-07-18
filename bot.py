c336c32e3957685dedea29e495e7e92780001e91
Mazkhan2025 <ghassemkh2024@gmail.com>
2025-07-18 14:26:28 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,18 +3,16 @@ from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
 TOKEN = "7607865658:AAGzPdwg6KeIC5YR_sxbTCehqOKD7UiRmoU"
 
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("سلام! ربات روشنه و داره جواب میده.")
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text("سلام! ربات فعال است.")
 
 def main():
     app = ApplicationBuilder().token(TOKEN).build()
-    
+
     app.add_handler(CommandHandler("start", start))
-    
-    print("ربات داره اجرا میشه...")
+
     app.run_polling()
 
 if __name__ == "__main__":
     main()
-
 
