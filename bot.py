1b4f3750464620e75382425e748a89604b18ade7
Mazkhan2025 <ghassemkh2024@gmail.com>
2025-07-18 14:09:49 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,15 +1,16 @@
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
-import os
 
-TOKEN = os.environ["BOT_TOKEN"]
+TOKEN = "7607865658:AAFYvhhjGygTCEu_sQWTAv5BnaOYQof8NQc"
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("سلام! به ربات خوش اومدی.")
+    await update.message.reply_text("سلام! ربات فعال شد.")
 
 def main():
     app = ApplicationBuilder().token(TOKEN).build()
+
     app.add_handler(CommandHandler("start", start))
+
     app.run_polling()
 
 if __name__ == "__main__":
